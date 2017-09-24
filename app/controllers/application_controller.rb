e32d74def0f5b532cf55e6d8f964b8eb706f183c
Nancy <belscher@nancys-mbp.attlocal.net>
2017-09-24 15:32:52 -0400

    I think I am done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,14 +5,14 @@ class ApplicationController < ActionController::Base
     # Available for use across all controllers
     def authenticate_user!
       if user_signed_in? != true
-        redirect_to sign_in_path
+        redirect_to new_session_path
       end
     end
 
     # method defined for controller to user
     # it will check if the current_user is present
     def user_signed_in?
-      User.find(session[:user_id]).present?
+      session[:user_id].present?
     end
 
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
       @books = Book.all
